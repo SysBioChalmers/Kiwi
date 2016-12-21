99781832d6a4c68de4b50ecf6a5a4ddf17e0591f
varemo <leifvaremo+git@gmail.com>
2016-12-21 15:24:29 +0100

    update version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='KiwiDist',
-    version='0.3.5',
+    version='0.3.6',
     author='F. Gatto and L. Väremo',
     author_email=['kiwi@sysbio.se'],
     packages=['kiwi'],
